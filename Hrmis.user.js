37e9a9dfd4de75b400580df235e120e07ca535f3
mac <last2003@gmail.com>
2016-10-13 14:37:03 +0800

    add js5

--- Hrmis.user.js
+++ Hrmis.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hrmis
 // @namespace    http://inner-portal.ccs.org.cn/hrmis/
-// @version      0.4(20161013)
+// @version      0.5(20161013)
 // @description  Just for fun!
 // @author       You
 // @match        http://inner-portal.ccs.org.cn/hrmis/exam/exam!examOnline.do?className=*
